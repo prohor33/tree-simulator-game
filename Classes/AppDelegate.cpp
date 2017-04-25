fff17f1080169d0e5d59078d1a5d66d0121aabae
prohor33 <prohorgladkikh@gmail.com>
2017-04-26 02:06:52 +0300

    ничего не усепл добавить

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -18,10 +18,10 @@ using namespace CocosDenshion;
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+static cocos2d::Size designResolutionSize = cocos2d::Size(480, 850);
+static cocos2d::Size smallResolutionSize = cocos2d::Size(320, 480);
+static cocos2d::Size mediumResolutionSize = cocos2d::Size(1080, 1920);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 2732);
 
 AppDelegate::AppDelegate()
 {
